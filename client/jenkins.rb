a05869367e26d5586d0b7178650bab2ea9be2827
Hisaaki Ito <hisa110@gmail.com>
2013-08-03 06:57:58 +0900

    angry jenkins mode

--- client/jenkins.rb
+++ client/jenkins.rb
@@ -6,6 +6,9 @@ DOMAIN="localhost:8080"
 JOBNAME="test"
 KOBO_HOST="192.168.1.100"
 SLEEP=10
+ANGRY_LIMIT=3
+
+err_cnt=0
 
 while true do
   response=RestClient::get "http://#{DOMAIN}/job/#{JOBNAME}/lastBuild/api/json?pretty=true"
@@ -17,10 +20,18 @@ while true do
   telnet = Net::Telnet.new("Host" => KOBO_HOST, "Timeout" => 10)
   
   if build_result == "SUCCESS"
-    telnet.cmd("cat /mnt/onboard/jenkins.raw  | /usr/local/Kobo/pickel showpic") 
+    jenkins="jenkins"
+    err_cnt=0
   else
-    telnet.cmd("cat /mnt/onboard/sad-jenkins.raw  | /usr/local/Kobo/pickel showpic") 
+    jenkins="sad-jenkins"
+    err_cnt=err_cnt+1
   end
+
+  if err_cnt >= ANGRY_LIMIT
+    jenkins="angry-jenkins"
+  end
+  
+  telnet.cmd("cat /mnt/onboard/#{jenkins}.raw  | /usr/local/Kobo/pickel showpic") 
 
   sleep SLEEP
 
